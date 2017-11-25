e3cc2543b768b540fe3137f9992b458f8843a3cf
Rohan Sahai <rohan@affinity.co>
2017-11-24 23:56:44 -0500

    api endpoints working, gin server

--- main.go
+++ main.go
@@ -2,44 +2,77 @@ package main
 
 import (
 	"fmt"
-	"encoding/json"
-	"log"
-	"net/http"
-	"github.com/gorilla/mux"
+	"time"
+	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
+var db *gorm.DB
+var err error
+
 type Fact struct {
-    gorm.Model
-    Text 	string
+	ID        uint   `json:"id"`
+    Text 	string   `json:"text"`
+    CreatedAt time.Time `json:"createdAt"`
+    DeletedAt time.Time `json:"deletedAt"`
 }
 
 
 // fun main()
 func main() {
-	router := mux.NewRouter()
-	router.HandleFunc("/facts", GetFacts).Methods("GET")
-	// router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
-	// router.HandleFunc("/people/{id}", CreatePerson).Methods("POST")
-	// router.HandleFunc("/people/{id}", DeletePerson).Methods("DELETE")
-	log.Fatal(http.ListenAndServe(":8000", router))
-}
-
-func GetFacts(w http.ResponseWriter, r *http.Request) {
 	// db connection
-	db, err := gorm.Open("postgres", "user=rohan dbname=fun_facts sslmode=disable")
+	db, err = gorm.Open("postgres", "user=rohan dbname=fun_facts sslmode=disable")
 
 	if err != nil {
 		fmt.Println(err)
 	}
 	defer db.Close()
 
-	facts := []Fact{}
-	db.Find(&facts)
+	db.AutoMigrate(&Fact{})
 
-	json.NewEncoder(w).Encode(facts)
+	r := gin.Default()
+	r.GET("/facts/", GetFacts)
+	r.GET("/facts/:id", GetFact)
+	r.POST("/facts", CreateFact)
+	r.DELETE("/facts/:id", DeleteFact)
+
+	r.Run(":8080")
 }
-// func GetPerson(w http.ResponseWriter, r *http.Request) {}
-// func CreatePerson(w http.ResponseWriter, r *http.Request) {}
-// func DeletePerson(w http.ResponseWriter, r *http.Request) {}
+
+func GetFacts(c *gin.Context) {
+	var facts []Fact
+	if err := db.Find(&facts).Error; err != nil {
+		c.AbortWithStatus(404)
+		fmt.Println(err)
+	} else {
+		c.JSON(200, facts)
+	}
+}
+
+func GetFact(c *gin.Context) {
+	id := c.Params.ByName("id")
+	var fact Fact
+	if err := db.Where("id = ?", id).First(&fact).Error; err != nil {
+		c.AbortWithStatus(404)
+		fmt.Println(err)
+	} else {
+		c.JSON(200, fact)
+	}
+}
+
+func CreateFact(c *gin.Context) {
+	var fact Fact
+	c.BindJSON(&fact)
+
+	db.Create(&fact)
+	c.JSON(200, fact)
+}
+
+func DeleteFact(c *gin.Context) {
+	id := c.Params.ByName("id")
+	var fact Fact
+	d := db.Where("id = ?", id).Delete(&fact)
+	fmt.Println(d)
+	c.JSON(200, gin.H{"id #" + id: "deleted"})
+}
