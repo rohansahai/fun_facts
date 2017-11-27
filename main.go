778fd01ecfa6f864ceb8fc9fd86b81d0bf5732fa
Rohan Sahai <rohan@affinity.co>
2017-11-26 23:14:38 -0500

    adds smarter db connection

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
@@ -20,7 +21,12 @@ type Fact struct {
 // fun main()
 func main() {
 	// db connection
-	db, err = gorm.Open("postgres", "user=rohan dbname=fun_facts sslmode=disable")
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+	dbName := os.Getenv("DB_NAME")
+	dbPwd := os.Getenv("DB_PWD")
+	dbString := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, dbUser, dbName, dbPwd)
+	db, err = gorm.Open("postgres", dbString)
 
 	if err != nil {
 		fmt.Println(err)
